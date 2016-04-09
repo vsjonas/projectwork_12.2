930a44479fed715d5387e949aa1183d17df9230b
cineafx <karl.neugebauer1997@gmail.com>
2016-04-09 10:55:03 +0000

    Erste Änderungen um Richtung und Geschwindigkeit zu übertragen

--- server/Main.java
+++ server/Main.java
@@ -12,26 +12,38 @@ public class Main {
 
   public static void main(String[] args){
     int pwmamount = 0;
-      int pinnr = 0;
-      Gpio.wiringPiSetup();             //setup the pi for pgio
-      SoftPwm.softPwmCreate(pinnr, 0, 100);     //create pwm  (pinnr, min, max)
-    
+    int pinnrL = 0;
+    int pinnrR = 1;
+    int power = 0;
+    String serverResponse = "0000";
+
+    Gpio.wiringPiSetup();             //setup the pi for pgio
+    SoftPwm.softPwmCreate(pinnrL, 0, 100);     //create pwm  (pinnr, min, max)
+    SoftPwm.softPwmCreate(pinnrR, 0, 100);   
+ 
     try
       {
         //Warte auf Anfragen auf Port 13000:
         ServerSocket serverSocket = new ServerSocket(13000); 
         System.out.println("Server: Socket open");
-        //Eine einzige Anfrage entgegennehmen:
+        //Eine einzige Anfrage entgegennehmen
         Socket clientSocket = serverSocket.accept();
         System.out.println("Server: Client connected");
-        while(pwmamount >= 0){
+        while(Integer.parseInt(serverResponse.substring(1,2)) != 1){
           BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));   //wait for an input Stream
-          String serverResponse = in.readLine();        //server answer to String
-          
-          pwmamount = Integer.parseInt(serverResponse);       //server answer to integer
-          SoftPwm.softPwmWrite(pinnr, pwmamount);       //set (pinnr, any number between min & max)
-          System.out.println("PWM set to: " + pwmamount);
-          Thread.sleep(25);
+	  serverResponse = in.readLine();        //reads new Response
+          System.out.println(serverResponse);
+
+	  pwmamount = Integer.parseInt(serverResponse.substring(0,1));       //server answer to integer 
+	  power = Integer.parseInt(serverResponse.substring(2,4));
+	 
+          //SoftPwm.softPwmWrite(pinnr, pwmamount);       //set (pinnr, any number between min & max)
+          //System.out.println("PWM set to: " + pwmamount);  
+	  
+	  System.out.println("direction: " + pwmamount);
+	  System.out.println("power: " + power);
+	  
+	  Thread.sleep(25);
         }
         serverSocket.close();
         System.out.println("Server: Socket closed");
