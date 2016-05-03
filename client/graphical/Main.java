dcfd26ea52d026e83ad1c89a5512811cce8b8e8f
cineafx <karl.neugebauer1997@gmail.com>
2016-05-03 13:48:19 +0200

    added the ping feature in the client

--- client/graphical/Main.java
+++ client/graphical/Main.java
@@ -3,12 +3,19 @@ import java.awt.event.*;
 import java.net.Socket;
 import java.io.PrintWriter;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main extends Frame implements ActionListener{
   // start attributes
-  Button bArr[] = new Button[9];
-  int power = 4;
+  static Button bArr[] = new Button[9];
+  static Label lStatus = new Label();
+  static TextField tIP = new TextField();
+  static int power = 4;
   static Socket socket;
   static PrintWriter out; 
+  static BufferedReader in; 
+  static boolean connected = false;
   // end attributes
   
   public Main(String title) { 
@@ -24,7 +31,7 @@ public class Main extends Frame implements ActionListener{
     int x = (d.width - getSize().width) / 2;
     int y = (d.height - getSize().height) / 2;
     setLocation(x, y);
-    setResizable(false);
+    setResizable(true);
     Panel cp2 = new Panel(new BorderLayout());
     add(cp2);
 
@@ -47,7 +54,11 @@ public class Main extends Frame implements ActionListener{
     bArr[6].setLabel("6");
     bArr[7].setLabel("5");
     bArr[8].setLabel("4");
-    
+
+    lStatus.setBackground(Color.LIGHT_GRAY);
+    lStatus.setText("Status: not connected");
+    tIP.setText("192.168.3.1");
+
     Button bConnect = new Button();
     bConnect.setLabel("connect");
     bConnect.addActionListener(this);
@@ -55,11 +66,12 @@ public class Main extends Frame implements ActionListener{
     bClose.setLabel("close");
     bClose.addActionListener(this);
     
-    Panel cp3 = new Panel(new GridLayout(1,2));
+    Panel cp3 = new Panel(new GridLayout(2,2));
     cp2.add(cp3, BorderLayout.NORTH);
+    cp3.add(lStatus);
+    cp3.add(tIP);
     cp3.add(bConnect);
     cp3.add(bClose);
-
 
     // start components   
     // end components
@@ -79,6 +91,9 @@ public class Main extends Frame implements ActionListener{
             System.out.println("closing down the socket");
             send("0100");
             socket.close();
+            lStatus.setBackground(Color.LIGHT_GRAY);
+            lStatus.setText("Status: not connected");
+            connected = false;
         }else if(action.equals("0")){                   //stop
             send("000");
         }else{                                          //move
@@ -90,23 +105,52 @@ public class Main extends Frame implements ActionListener{
         }
     }catch(Exception ex){
         ex.printStackTrace();
+        lStatus.setBackground(Color.RED);
+        lStatus.setText("Status: failed");
+        connected = false;
     }
   }
   public static void reconnect(){
     try{
+        
         System.out.println("Client: Searching for Socket");
-        socket = new Socket ("192.168.3.1", 13000);  //ip address and port of the server 
+        socket = new Socket (tIP.getText(), 13000);  //ip address and port of the server 
         out = new PrintWriter(socket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         System.out.println("Connected");
+        lStatus.setBackground(Color.GREEN);
+        lStatus.setText("Status: connected");
+        connected = true;       
     }catch(Exception ex){
         ex.printStackTrace();
+        lStatus.setBackground(Color.RED);
+        lStatus.setText("Status: failed");
+        connected = false;
     }
   }
   public static void send(String message){
       out.println(message);
   }
+
   public static void main(String[] args) {
-    new Main("Main");
+    new Main("Main");        
+    while(true){
+        
+         try{
+            Thread.sleep(1000);
+            if(connected){
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String message = in.readLine();
+                if(message.equals("ping")){
+                    send("0300");
+                    System.out.println("ping answered");
+                }
+            Thread.sleep(1000);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    } 
   } // end of main
     
     // end methods
